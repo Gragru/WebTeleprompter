4fc727449cd55617dadf71ca87d764b86229af2a
Gragru <micke@ptimat.se>
2025-01-22 13:33:04 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,61 @@
+var speed = 1;
+var time = 60;
+var scroll = true;
+var sX = 1;
+
+var element = document.getElementById('text');
+
+element.scrollTop = -100;
+
+function scrollElementBy50px(){
+    
+    console.log(element.scrollTop)
+    if(scroll == true)
+    {
+        console.log("Scroll");
+        element.scrollTop += speed;
+    }
+
+    if (element.scrollTop + element.clientHeight >= element.scrollHeight) {
+        console.log("End of scroll");
+        scroll = false;
+    }
+
+    if(scroll == true)
+    {
+        document.getElementById('speedometer').innerHTML = time;
+        setTimeout(scrollElementBy50px, time);
+    }
+
+}
+
+
+function Increase(){
+    time -= 5;
+    console.log(time);
+}
+
+function Decrease(){
+    time += 5;
+    console.log(time);
+}
+
+function Reset(){
+    element.scrollTop = -10000;
+    scroll = true;
+}
+
+function Mirror(){
+
+    if(sX == 1) 
+    {
+        sX = -1;
+        element.className = "container1";
+    }
+    else
+    {
+        sX = 1;
+        element.className = "container2";
+    }
+    console.log(sX);
+}
